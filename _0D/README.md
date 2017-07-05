75639c724eaa7f31ab772d938fb9a6a7a05907b1
deekshakoul <deekshakoul.daiict@gmail.com>
2017-07-05 12:12:52 +0530

    Changed Readme and makefile

--- _0D/README.md
+++ _0D/README.md
@@ -1,7 +1,29 @@
 ========================
 
 Code for 0D simulation
-
 ========================
 
+The code uses:
+1. Anukalana ( https://github.com/sivaramambikasaran/Anukalana )
+2. The Eigen library ( eigen.tuxfamily.org/ )
+
+
+## 1. Providing the input data:
+	1a. Copy the thermo file (.thermo), the transport file(.trans) and the mechanism file(.mech) into the folder: inputFiles
+
+	1b. Open the file 'allInput.txt' to enter the input file names, and the other initial conditions as mentioned in the file.
+
+## 2. Running a test
+Proceed as follows:
+	2a. First clean the directory:
+		make -f makefile.mk clean
+
+	2b. Make the makefile:
+		make -f makefile.mk
+
+	2c. Run the code as:
+		./exec/test N
+
+	where `N` is the number of timesteps for the problem. For instance,
+		./exec/test 10
 
--- _0D/makefile.mk
+++ _0D/makefile.mk
@@ -1,10 +1,12 @@
 CC	=g++-5 -std=c++11
-CFLAGS	=-c -Wall -Ofast -ffast-math -ffinite-math-only -I header/
+CFLAGS	=-c -g -Wall -Ofast -ffast-math -ffinite-math-only -I header/ 
 LDFLAGS	=
-SOURCES	=./Anukanala-master/src/RK_NonAdaptive.cpp ./Anukanala-master/src/RK_Adaptive.cpp ./Anukanala-master/src/Adambashforth.cpp ./Anukanala-master/src/Euler_Implicit.cpp ./Anukanala-master/src/LeapFrog.cpp ./src/Isobaric1.cpp ./src/Isochoric1.cpp ./src/Properties1.cpp ./examples/test.cpp
+SOURCES	= ./src/Isobaric1.cpp ./src/Isochoric1.cpp ./src/Properties1.cpp ./examples/test.cpp
 OBJECTS	=$(SOURCES:.cpp=.o)
 EXECUTABLE	=./exec/test
 
+subsystem:
+	$(MAKE) -C ./../Anukalana
 all: $(SOURCES) $(EXECUTABLE)
 
 $(EXECUTABLE): $(OBJECTS)
@@ -14,7 +16,7 @@ $(EXECUTABLE): $(OBJECTS)
 	$(CC) $(CFLAGS) $(KERNEL) $(DIM) $< -o $@
 
 clean:
-	rm -rf *.out ./examples/*.o ./src/*.o ./exec/* ./Anukalana-master/src/*.o
+	rm -rf *.out ./examples/*.o ./src/*.o ./exec/* 
 
 tar:
 	tar -zcvf Anukalana.tar.gz ./makefile.mk ./exec ./src ./header ./examples ./README.md ./LICENSE.md
